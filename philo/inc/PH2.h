940af5c0e1a0c7c08d54abf7d47ea1a1b041d8e2
rodrigo-fari <rodrigofal44@gmail.com>
2025-06-04 23:45:13 +0100

    Struct redefinition finish.

--- philo/inc/PH2.h
+++ philo/inc/PH2.h
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 12:42:33 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/06/04 22:13:18 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/06/04 23:44:51 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,41 @@ typedef struct s_philo	t_philo;
 
 typedef struct s_global
 {
-
+	t_data			*data;//Program general info.
+	t_table			*table;//Table general info.
 }	t_global;
 
 typedef struct s_data
 {
-
+	int				nop;//Number of philosophers.
+	int				ttd;//Time to die.
+	int				tte;//Time to eat.
+	int				tts;//Time to sleep.
+	int				nod;//Number of dishes.
 }	t_data;
 
 typedef struct s_table
 {
-
+	long			start_time;//Save the dinner starting time.
+	bool			dead_philo;//Set philo dead.
+	t_philo			*philos;//Philosophers general info.
+	pthread_t		monitor_thread;//Check philosopher's integrity.
+	pthread_mutex_t	*msg_lock;//Lock -> Log message.
+	pthread_mutex_t	*fork_lock;//Lock -> Forks.
+	pthread_mutex_t	*dead_philo_lock;//Lock -> dead_philo flag.
 }	t_table;
 
 typedef struct s_philo
 {
-
+	int				id;//Philosopher id.
+	long			last_meal;//Time since last meal.
+	t_data			*data;//Data general info.
+	t_table			*table;//Table general info.
+	pthread_t		thread_id;//Philosopher DNA ;p
+	pthread_mutex_t	*l_fork;//Lock -> Left fork.
+	pthread_mutex_t	*r_fork;//Lock -> Right fork.
+	pthread_mutex_t	*meal_lock;//Lock -> Meal count.
+	pthread_mutex_t	*last_meal_lock;//Lock -> las_meal var.
 }	t_philo;
 
 
